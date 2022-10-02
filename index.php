bafe9a3dd9e235688accf15123f580b635483474
777dan <danya.nikitin7@gmail.com>
2022-10-02 16:28:53 +0300

    second commit

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
 $str1 = "'''''";
 $str2 = "''''";
-$check = substr_count($str1, "'");
-if ($check % 2 == 0) {
-    echo "Everything is right!";
-} else {
-    echo "Error!";
+$str3 = "())";
+$str4 = "()()";
+$strings = [$str1, $str2, $str3, $str4];
+function right($str){
+    echo "Everything is right! [$str]; ";
+}
+function false($str){
+    echo "Error! [$str]; ";
+}
+for ($i = 0; $i < count($strings); $i++) {
+    if (gettype(strpos($strings[$i], "'")) === "integer") {
+        $check = substr_count($strings[$i], "'");
+        if ($check % 2 == 0) {
+            right($strings[$i]);
+        } else {
+            false($strings[$i]);
+        }
+    } else {
+        $check1 = substr_count($strings[$i], ")");
+        $check2 = substr_count($strings[$i], "(");
+        if ($check1 == $check2) {
+            right($strings[$i]);
+        } else {
+            false($strings[$i]);
+        }
+    }
 }
