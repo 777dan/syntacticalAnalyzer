606c6f8ced69b6ad907f87cd50864269f4a0f405
777dan <danya.nikitin7@gmail.com>
2022-10-02 18:20:14 +0300

    third commit

--- index.php
+++ index.php
@@ -8,17 +8,18 @@ function right($str){
     echo "Everything is right! [$str]; ";
 }
 function false($str){
-    echo "Error! [$str]; ";
+    echo "Not everything is right! [$str]; ";
 }
 for ($i = 0; $i < count($strings); $i++) {
-    if (gettype(strpos($strings[$i], "'")) === "integer") {
+    if (strpos($strings[$i], "'") !== false) {
         $check = substr_count($strings[$i], "'");
         if ($check % 2 == 0) {
             right($strings[$i]);
         } else {
             false($strings[$i]);
         }
-    } else {
+    } 
+    if (strpos($strings[$i], ")") !== false or strpos($strings[$i], "(") !== false) {
         $check1 = substr_count($strings[$i], ")");
         $check2 = substr_count($strings[$i], "(");
         if ($check1 == $check2) {
